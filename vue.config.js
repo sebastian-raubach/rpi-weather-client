4d904084c899a3be3dd31dc7595c45e82af16856
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2021-04-25 18:02:11 +0100

    - ADD: Added default Vue app for testing.

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// 
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  pwa: {
+    name: 'RPI-Weather',
+    themeColor: '#325D88',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    start_url: '/index.html',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
+  // configureWebpack: {
+  //   plugins: [
+  //     new BundleAnalyzerPlugin()
+  //   ]
+  // }
+}
