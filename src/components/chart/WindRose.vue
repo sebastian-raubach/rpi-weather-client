6f3a635b85d478fb027a164c832262a20803f99d
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2021-05-13 08:16:05 +0100

    - FIX: Fixed layout of wind rose on mobile devices. - CHG: Changed layout of wind rose to be more aligned with other visualizations.

---
+++ src/components/chart/WindRose.vue
@@ -0,0 +1,152 @@
+<template>
+  <b-row class="my-4">
+    <b-col cols=12 lg=10>
+      <div ref="windChart" />
+    </b-col>
+    <b-col cols=12 lg=2 class="h-100 order-first order-lg-last">
+      <b-card no-body class="text-center h-100 mb-4">
+        <b-card-header>
+          <h1 :style="{ color: '#006266' }"><i class="bi-compass" /></h1>
+        </b-card-header>
+        <b-card-body class="h-100">
+          <h3>{{ currentWind }}</h3>
+        </b-card-body>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import RangeMap from '@/plugin/rangemap'
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data: function () {
+    return {
+      currentWind: null,
+      wd: ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'],
+      windDirections: new RangeMap([
+        { min: 337.5, max: 22.5, value: 'N', mode: 'invert' },
+        { min: 22.5, max: 67.5, value: 'NE' },
+        { min: 67.5, max: 112.5, value: 'E' },
+        { min: 112.5, max: 157.5, value: 'SE' },
+        { min: 157.5, max: 202.5, value: 'S' },
+        { min: 202.5, max: 247.5, value: 'SW' },
+        { min: 247.5, max: 292.5, value: 'W' },
+        { min: 292.5, max: 337.5, value: 'NW' }
+      ]),
+      windCategories: new RangeMap([
+        { min: 0, max: 1, value: { name: 'Calm', color: '#E3F2FD' } },
+        { min: 1, max: 6, value: { name: 'Light Air', color: '#BBDEFB' } },
+        { min: 6, max: 12, value: { name: 'Light Breeze', color: '#90CAF9' } },
+        { min: 12, max: 20, value: { name: 'Gentle Breeze', color: '#64B5F6' } },
+        { min: 20, max: 30, value: { name: 'Moderate Breeze', color: '#42A5F5' } },
+        { min: 30, max: 40, value: { name: 'Fresh Breeze', color: '#2196F3' } },
+        { min: 40, max: 50, value: { name: 'Strong Breeze', color: '#1E88E5' } },
+        { min: 50, max: 62, value: { name: 'Near Gale', color: '#1976D2' } },
+        { min: 62, max: 75, value: { name: 'Gale', color: '#1565C0' } },
+        { min: 75, max: 89, value: { name: 'Strong Gale', color: '#0D47A1' } },
+        { min: 89, max: 103, value: { name: 'Storm', color: '#D32F2F' } },
+        { min: 103, max: 118, value: { name: 'Violent Storm', color: '#C62828' } },
+        { min: 118, max: Number.MAX_SAFE_INTEGER, value: { name: 'Hurricane', color: '#B71C1C' } }
+      ])
+    }
+  },
+  watch: {
+    data: function () {
+      this.$nextTick(() => {
+        this.update()
+      })
+    }
+  },
+  methods: {
+    update: function () {
+      const div = this.$refs.windChart
+
+      if (div) {
+        this.$plotly.purge(div)
+      }
+
+      if (this.data && this.data.length > 0) {
+        const counts = this.windCategories.ranges.map(r => { return { name: r.value.name, color: r.value.color, counts: this.wd.map(_ => 0), total: 0 } })
+
+        this.data.filter(d => d.windAverage !== undefined && d.windAverage !== null && d.windSpeed !== undefined && d.windSpeed !== null)
+          .forEach(d => {
+            const speed = d.windSpeed
+            const direction = d.windAverage
+
+            const speedCategory = this.windCategories.getValue(speed).name
+            const directionCategory = this.windDirections.getValue(direction)
+
+            const counter = counts.find(c => c.name === speedCategory)
+
+            if (counter) {
+              counter.counts[this.wd.indexOf(directionCategory)]++
+              counter.total++
+            }
+          })
+
+        const winds = this.data.filter(d => d.windAverage !== undefined && d.windAverage !== null && d.windSpeed !== undefined && d.windSpeed !== null)
+
+        if (winds.length > 0) {
+          this.currentWind = this.windDirections.getValue(winds[winds.length - 1].windAverage)
+        } else {
+          this.currentWind = null
+        }
+
+        const data = counts.filter(c => c.total > 0).map(c => {
+          return {
+            r: c.counts,
+            theta: this.wd,
+            name: c.name,
+            marker: {
+              color: c.color
+            },
+            type: 'barpolar'
+          }
+        })
+
+        const layout = {
+          height: 600,
+          margin: { autoexpand: true },
+          dragmode: false,
+          autosize: true,
+          paper_bgcolor: '#4e5d6c',
+          plot_bgcolor: 'rgba(1.0, 1.0, 1.0, 0.1)',
+          legend: { font: { color: 'white' }, orientation: 'h' },
+          polar: {
+            bgcolor: 'rgba(1.0, 1.0, 1.0, 0.1)',
+            radialaxis: { visible: false },
+            angularaxis: {
+              direction: 'clockwise',
+              tickfont: { color: 'white' }
+            }
+          },
+          font: { size: 16 },
+          orientation: 270
+        }
+
+        const config = {
+          displayModeBar: false,
+          responsive: true,
+          displaylogo: false
+        }
+
+        this.$plotly.newPlot(div, data, layout, config)
+      }
+    }
+  },
+  mounted: function () {
+    this.update()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -10,8 +10,6 @@
     <div v-if="dataFile && dataFile.length > 0">
       <h2 class="my-3">Last measurement: {{ lastMeasurementDateTime }}</h2>
 
-      <div ref="windChart" />
-
       <b-row v-for="(variable, index) in variables" :key="`variable-${index}`" class="my-4">
         <b-col cols=12 lg=10>
           <b-card>
@@ -33,12 +31,15 @@
           </b-row>
         </b-col>
       </b-row>
+
+      <WindRose :data="dataFile" />
     </div>
   </div>
 </template>
 
 <script>
 import LineChart from '@/components/chart/LineChart'
+import WindRose from '@/components/chart/WindRose'
 
 import RangeMap from '@/plugin/rangemap'
 
@@ -47,7 +48,8 @@ const SunCalc = require('suncalc')
 export default {
   name: 'Dashboard',
   components: {
-    LineChart
+    LineChart,
+    WindRose
   },
   data: function () {
     return {
@@ -85,40 +87,7 @@ export default {
         { min: 0.625, max: 0.750, value: { name: 'Waning Gibbous', icon: 'bi-circle-fill' } },
         { min: 0.750, max: 0.875, value: { name: 'Last Quarter', icon: 'bi-circle-half icon-flipped' } },
         { min: 0.875, max: 1.000, value: { name: 'Waning Crescent', icon: 'bi-circle-half icon-flipped' } }
-      ]),
-      wd: ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'],
-      windDirections: new RangeMap([
-        { min: 337.5, max: 22.5, value: 'N', mode: 'invert' },
-        { min: 22.5, max: 67.5, value: 'NE' },
-        { min: 67.5, max: 112.5, value: 'E' },
-        { min: 112.5, max: 157.5, value: 'SE' },
-        { min: 157.5, max: 202.5, value: 'S' },
-        { min: 202.5, max: 247.5, value: 'SW' },
-        { min: 247.5, max: 292.5, value: 'W' },
-        { min: 292.5, max: 337.5, value: 'NW' }
-      ]),
-      windCategories: new RangeMap([
-        { min: 0, max: 1, value: { name: 'Calm', color: '#E3F2FD' } },
-        { min: 1, max: 6, value: { name: 'Light Air', color: '#BBDEFB' } },
-        { min: 6, max: 12, value: { name: 'Light Breeze', color: '#90CAF9' } },
-        { min: 12, max: 20, value: { name: 'Gentle Breeze', color: '#64B5F6' } },
-        { min: 20, max: 30, value: { name: 'Moderate Breeze', color: '#42A5F5' } },
-        { min: 30, max: 40, value: { name: 'Fresh Breeze', color: '#2196F3' } },
-        { min: 40, max: 50, value: { name: 'Strong Breeze', color: '#1E88E5' } },
-        { min: 50, max: 62, value: { name: 'Near Gale', color: '#1976D2' } },
-        { min: 62, max: 75, value: { name: 'Gale', color: '#1565C0' } },
-        { min: 75, max: 89, value: { name: 'Strong Gale', color: '#0D47A1' } },
-        { min: 89, max: 103, value: { name: 'Storm', color: '#D32F2F' } },
-        { min: 103, max: 118, value: { name: 'Violent Storm', color: '#C62828' } },
-        { min: 118, max: Number.MAX_SAFE_INTEGER, value: { name: 'Hurricane', color: '#B71C1C' } }
       ])
-    }
-  },
-  watch: {
-    dataFile: function () {
-      this.$nextTick(() => {
-        this.updateWind()
-      })
     }
   },
   computed: {
@@ -187,79 +156,6 @@ export default {
       const moon = SunCalc.getMoonIllumination(new Date())
 
       this.moonPhase = this.moonPhases.getValue(moon.phase)
-    },
-    updateWind: function () {
-      const div = this.$refs.windChart
-
-      if (div) {
-        this.$plotly.purge(div)
-      }
-
-      if (this.dataFile && this.dataFile.length > 0) {
-        const counts = this.windCategories.ranges.map(r => { return { name: r.value.name, color: r.value.color, counts: this.wd.map(_ => 0) } })
-
-        this.dataFile.filter(d => d.windAverage !== undefined && d.windAverage !== null && d.windSpeed !== undefined && d.windSpeed !== null)
-          .forEach(d => {
-            const speed = d.windSpeed
-            const direction = d.windAverage
-
-            const speedCategory = this.windCategories.getValue(speed).name
-            const directionCategory = this.windDirections.getValue(direction)
-
-            const counter = counts.find(c => c.name === speedCategory)
-
-            if (counter) {
-              counter.counts[this.wd.indexOf(directionCategory)]++
-            }
-          })
-
-        const data = counts.map(c => {
-          return {
-            r: c.counts,
-            theta: this.wd,
-            name: c.name,
-            marker: {
-              color: c.color
-            },
-            type: 'barpolar'
-          }
-        })
-
-        const layout = {
-          margin: { autoexpand: true },
-          dragmode: false,
-          autosize: true,
-          paper_bgcolor: '#4e5d6c',
-          plot_bgcolor: 'rgba(1.0, 1.0, 1.0, 0.1)',
-          legend: { font: { color: 'white' } },
-          polar: {
-            bgcolor: 'rgba(1.0, 1.0, 1.0, 0.1)',
-            radialaxis: {
-              gridcolor: 'rgba(1.0, 1.0, 1.0, 0.1)',
-              tickfont: { color: 'white' }
-            },
-            angularaxis: {
-              direction: 'clockwise',
-              tickfont: { color: 'white' }
-            }
-          },
-          font: { size: 16 },
-          orientation: 270
-        }
-
-        const config = {
-          displayModeBar: false,
-          responsive: true,
-          displaylogo: false
-        }
-
-        this.$plotly.newPlot(div, data, layout, config)
-
-        div.on('plotly_hover', data => {
-          const label = data.points['0'].fullData.marker
-          label.color = '#000000'
-        })
-      }
     }
   },
   mounted: function () {
